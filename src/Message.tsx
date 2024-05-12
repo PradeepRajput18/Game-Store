752d175a99bc15a6193288f583ad954088a6e10c
bruteforce <rekkapallipradeepG@gmail.com>
2024-05-12 19:17:43 -0400

    Made Changes in File GG

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+
+
+function Message(){
+    const guru="Guruji"
+    if(guru) 
+        return <h1>Happy Birthday {guru}</h1>;
+    else return <h1>He is there always</h1>
+};
+
+export default Message;
